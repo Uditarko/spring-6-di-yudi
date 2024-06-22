abe2ddebc75cbceab52464cdf967454a40691bab
Uditarko Dutta <uditarko.dutta@gmail.com>
2024-06-22 15:57:03 +0530

    using profiles

---
+++ src/main/java/guru/springframework/yudi/spring6diyudi/services/i18n/EnglishGreetingServiceImpl.java
@@ -0,0 +1,14 @@
+package guru.springframework.yudi.spring6diyudi.services.i18n;
+
+import guru.springframework.yudi.spring6diyudi.services.GreetingService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("EN")
+@Service("i18NService")
+public class EnglishGreetingServiceImpl implements GreetingService {
+    @Override
+    public String sayHello() {
+        return "Hello in - EN";
+    }
+}
