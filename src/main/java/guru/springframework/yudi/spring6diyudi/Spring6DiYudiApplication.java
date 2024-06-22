59e7ad41a0472caebed3899028e774fc01d35fb8
Uditarko Dutta <uditarko.dutta@gmail.com>
2024-06-22 13:41:13 +0530

    created MyController and getting it from ctx

--- src/main/java/guru/springframework/yudi/spring6diyudi/Spring6DiYudiApplication.java
+++ src/main/java/guru/springframework/yudi/spring6diyudi/Spring6DiYudiApplication.java
@@ -1,13 +1,16 @@
 package guru.springframework.yudi.spring6diyudi;
 
+import guru.springframework.yudi.spring6diyudi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring6DiYudiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring6DiYudiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(Spring6DiYudiApplication.class, args);
+		System.out.println(ctx.getBean(MyController.class).sayHello());
 	}
 
 }
---
+++ src/main/java/guru/springframework/yudi/spring6diyudi/controllers/MyController.java
@@ -0,0 +1,11 @@
+package guru.springframework.yudi.spring6diyudi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello(){
+        return "Hello Everyone!!";
+    }
+}
