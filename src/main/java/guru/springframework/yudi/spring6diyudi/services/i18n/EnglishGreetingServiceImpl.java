f075351dd9745f6a5e6ccf7d7568d71273c6e8fe
Uditarko Dutta <uditarko.dutta@gmail.com>
2024-06-22 16:02:36 +0530

    using profiles

--- src/main/java/guru/springframework/yudi/spring6diyudi/services/i18n/EnglishGreetingServiceImpl.java
+++ src/main/java/guru/springframework/yudi/spring6diyudi/services/i18n/EnglishGreetingServiceImpl.java
@@ -4,7 +4,7 @@ import guru.springframework.yudi.spring6diyudi.services.GreetingService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN","default"})
 @Service("i18NService")
 public class EnglishGreetingServiceImpl implements GreetingService {
     @Override
