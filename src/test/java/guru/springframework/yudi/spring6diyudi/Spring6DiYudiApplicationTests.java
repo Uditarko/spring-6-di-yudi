d793c019f8a770888f4b9af5a8daa9caee90956f
Uditarko Dutta <uditarko.dutta@gmail.com>
2024-06-22 13:46:05 +0530

    testing myController injection in test

--- src/test/java/guru/springframework/yudi/spring6diyudi/Spring6DiYudiApplicationTests.java
+++ src/test/java/guru/springframework/yudi/spring6diyudi/Spring6DiYudiApplicationTests.java
@@ -1,13 +1,32 @@
 package guru.springframework.yudi.spring6diyudi;
 
+import guru.springframework.yudi.spring6diyudi.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DiYudiApplicationTests {
 
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController myController;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testGetControllerFromContext(){
+		System.out.println(applicationContext.getBean(MyController.class).sayHello());
+	}
+
+	@Test
+	public void testInjectedController(){
+		System.out.println(myController.sayHello());
 	}
 
 }
