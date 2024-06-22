c9e1b6b7d1098d4d810d0df38f89ef9bc21be6ac
Uditarko Dutta <uditarko.dutta@gmail.com>
2024-06-22 12:58:39 +0530

    Initial Commit

---
+++ src/main/java/guru/springframework/yudi/spring6diyudi/Spring6DiYudiApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.yudi.spring6diyudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6DiYudiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6DiYudiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/yudi/spring6diyudi/Spring6DiYudiApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.yudi.spring6diyudi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6DiYudiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
